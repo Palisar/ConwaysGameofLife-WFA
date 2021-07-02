4bcee7daf4f1ebbb645982b0107b08d34bdffa20
Palisar <83838480+Palisar@users.noreply.github.com>
2021-07-02 18:44:18 +0100

    Add project files.

---
+++ ConwaysGameofLife WFA/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace ConwaysGameofLife_WFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(492, 346);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.picPaint);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(516, 370);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ ConwaysGameofLife WFA/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConwaysGameofLife_WFA
+{
+    public partial class Form1 : Form
+    {
+        Bitmap gameBitmap;
+        Random r = new Random();
+        int maxSize = 50;
+        bool[,] cell;
+
+        public Form1()
+        {
+            InitializeComponent();
+            initCells();
+        }
+        void initCells()
+        {
+            cell = new bool[maxSize, maxSize];
+            for (int i = 0; i < maxSize; i++)
+            {
+                for (int j = 0; j < maxSize; j++)
+                {
+                    cell[i, j] = (r.Next(2) == 0);
+                }
+            }
+            gameBitmap = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            timer1.Enabled = true;
+        }
+        
+        void mainLoop()
+        {
+            timer1.Enabled = false;
+            pictureBox2.Refresh(); 
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            mainLoop();
+        }
+        
+        private void picPaint(object sender, PaintEventArgs e)
+        {
+            //this using method will allow us to fill in out bitmap. with black squares
+            //    using (Graphics g = Graphics.FromImage(pictureBox2.Image))
+            Graphics g = Graphics.FromImage(gameBitmap);  
+            {
+                int w = 4;
+                    
+                for (int i = 0; i < maxSize; i++)
+                {
+                    for (int j = 0; j < maxSize; j++)
+                    {
+                        if (cell[i, j]) g.FillRectangle(Brushes.Black, i * w, j * w , w , w);
+                        else g.FillRectangle(Brushes.White , i * w, j * w, w, w);
+                    }
+                }
+            }
+            g.Dispose();
+
+            pictureBox2.Image = gameBitmap;
+            //this.Text = "Conways Game of Life";
+
+            cell = new bool[maxSize, maxSize];
+            for (int i = 0; i < maxSize; i++)
+            {
+                for (int j = 0; j < maxSize; j++)
+                {
+                    cell[i, j] = (r.Next(2) == 0);
+                }
+            }
+            timer1.Start();
+        }
+
+        
+    }
+
+}
