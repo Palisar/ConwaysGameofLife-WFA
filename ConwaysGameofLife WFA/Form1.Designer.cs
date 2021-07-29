30158bcfb3790e408cd0cb66fb1c51e97afcefda
Palisar <83838480+Palisar@users.noreply.github.com>
2021-07-29 20:12:17 +0100

    Finally finished my GAME OF LIFE

--- ConwaysGameofLife WFA/Form1.Designer.cs
+++ ConwaysGameofLife WFA/Form1.Designer.cs
@@ -32,13 +32,12 @@ namespace ConwaysGameofLife_WFA
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // timer1
-            // 
-            this.timer1.Interval = 1000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // pictureBox2
             // 
@@ -49,21 +48,68 @@ namespace ConwaysGameofLife_WFA
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.picPaint);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(530, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Randomize";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(551, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Speed";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(530, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 22);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(578, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 22);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(516, 370);
+            this.ClientSize = new System.Drawing.Size(633, 370);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox2);
             this.Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- ConwaysGameofLife WFA/Form1.cs
+++ ConwaysGameofLife WFA/Form1.cs
@@ -2,48 +2,91 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ConwaysGameofLife_WFA
 {
+    public enum Status
+    {
+        Alive,
+        Dead,
+    }
     public partial class Form1 : Form
     {
         Bitmap gameBitmap;
         Random r = new Random();
-        int maxSize = 50;
-        bool[,] cell;
-
+        const int maxSize = 50;
+        Status[,] cellGrid;
+        Status[,] nextGen;
+        int index = 0;
+        int speed = 30;
+        
         public Form1()
         {
             InitializeComponent();
             initCells();
+            
+        }
+        public static Status[,] NextGeneration(Status[,] cellGrid)
+        {
+            var nextGen = new Status[maxSize, maxSize];
+            for (int row = 1; row < maxSize -1 ; row++)
+            {
+                for (int col = 1; col < maxSize -1; col++)
+                {
+                    int neighbours = 0;
+                    for (int i = -1; i < 2; i++)
+                    {
+                        for (int j = -1; j < 2; j++)
+                        {
+                            neighbours += cellGrid[row + i, col + j] == Status.Alive ? 1 : 0;
+                        }
+                        
+                    }
+                    var currentCell = cellGrid[row, col];
+                    neighbours -= currentCell == Status.Alive ? 1 : 0;
+
+                    //check for under population
+                    if (currentCell == Status.Alive && neighbours < 2)
+                    {
+                        nextGen[row,col] = Status.Dead;
+                    }
+                    //check for over population
+                    else if (currentCell == Status.Alive && neighbours > 3)
+                    {
+                        nextGen[row, col] = Status.Dead;
+                    }
+                    //check to be born
+                    else if(currentCell == Status.Dead && neighbours == 3)
+                    {
+                        nextGen[row, col] = Status.Alive;
+                    }
+                    else
+                    {
+                        nextGen[row, col] = currentCell; 
+                    }
+                }
+            }
+            return nextGen;
         }
         void initCells()
         {
-            cell = new bool[maxSize, maxSize];
+            cellGrid = new Status[maxSize, maxSize];
             for (int i = 0; i < maxSize; i++)
             {
                 for (int j = 0; j < maxSize; j++)
                 {
-                    cell[i, j] = (r.Next(2) == 0);
+                    cellGrid[i, j] =  (Status)(r.Next(2));
                 }
             }
             gameBitmap = new Bitmap(pictureBox2.Width, pictureBox2.Height);
-            timer1.Enabled = true;
-        }
-        
-        void mainLoop()
-        {
-            timer1.Enabled = false;
-            pictureBox2.Refresh(); 
-        }
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            mainLoop();
+            nextGen = NextGeneration(cellGrid);
         }
         
         private void picPaint(object sender, PaintEventArgs e)
@@ -52,34 +95,60 @@ namespace ConwaysGameofLife_WFA
             //    using (Graphics g = Graphics.FromImage(pictureBox2.Image))
             Graphics g = Graphics.FromImage(gameBitmap);  
             {
-                int w = 4;
-                    
-                for (int i = 0; i < maxSize; i++)
+                int w = 6;
+                if (index == 1)
                 {
-                    for (int j = 0; j < maxSize; j++)
+                    for (int i = 0; i < maxSize; i++)
                     {
-                        if (cell[i, j]) g.FillRectangle(Brushes.Black, i * w, j * w , w , w);
-                        else g.FillRectangle(Brushes.White , i * w, j * w, w, w);
+                        for (int j = 0; j < maxSize; j++)
+                        {
+                            if (nextGen[i, j] == Status.Alive) g.FillRectangle(Brushes.Black, i * w, j * w, w, w);
+                            else g.FillRectangle(Brushes.White, i * w, j * w, w, w);
+                            
+                        }
                     }
+                    index = 0;
+                    cellGrid = NextGeneration(nextGen);
+                    Debug.WriteLine("nextGen population was used");
                 }
+                else
+                {
+                    for (int i = 0; i < maxSize; i++)
+                    {
+                        for (int j = 0; j < maxSize; j++)
+                        {
+                            if (cellGrid[i, j] == Status.Alive) g.FillRectangle(Brushes.Black, i * w, j * w, w, w);
+                            else g.FillRectangle(Brushes.White, i * w, j * w, w, w);
+                            
+                        }
+                    }
+                    index++;
+                     nextGen = NextGeneration(cellGrid);
+                    Debug.WriteLine("cellGrid population was used");
+                }
+               Thread.Sleep(speed);
             }
             g.Dispose();
 
             pictureBox2.Image = gameBitmap;
-            //this.Text = "Conways Game of Life";
+            this.Text = "Conways Game of Life";
 
-            cell = new bool[maxSize, maxSize];
-            for (int i = 0; i < maxSize; i++)
-            {
-                for (int j = 0; j < maxSize; j++)
-                {
-                    cell[i, j] = (r.Next(2) == 0);
-                }
-            }
-            timer1.Start();
         }
 
-        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            initCells();    
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            speed++;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            speed--;
+        }
     }
 
 }
